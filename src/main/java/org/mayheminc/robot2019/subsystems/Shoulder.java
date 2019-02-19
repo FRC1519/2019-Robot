3da5ed5715d55addbe7d8966543c15cf0ba39754
GabrielDeml <gabrielddeml@gmail.com>
2019-02-18 18:07:22 -0600

    Fixed things.

--- src/main/java/org/mayheminc/robot2019/subsystems/Shoulder.java
+++ src/main/java/org/mayheminc/robot2019/subsystems/Shoulder.java
@@ -21,9 +21,9 @@ public class Shoulder extends Subsystem {
     private static int IN_POSITION_SLOP = 100;
     private static double BRAKE_RELASE_TIME_SEC = 0.1;
 
-    private MayhemTalonSRX motor = new MayhemTalonSRX(RobotMap.SHOULDER_TALON_A);
+    private MayhemTalonSRX motor_A = new MayhemTalonSRX(RobotMap.SHOULDER_TALON_A);
+    private MayhemTalonSRX motor_B = new MayhemTalonSRX(RobotMap.SHOULDER_TALON_B);
     private Solenoid brake = new Solenoid(RobotMap.SHOULDER_BRAKE_SOLENOID);
-    private boolean percentState = false; 
 
     private int m_pos;
 
@@ -37,15 +37,18 @@ public class Shoulder extends Subsystem {
 
     public Shoulder() {
 
-        motor.config_kP(0, 1.0, 0);
-        motor.config_kI(0, 0.0, 0);
-        motor.config_kD(0, 0.0, 0);
-        motor.config_kF(0, 0.0, 0);
+        motor_B.changeControlMode(ControlMode.Follower);
+        motor_B.set(motor_A.getDeviceID());
 
-        motor.setNeutralMode(NeutralMode.Coast);
-        motor.configNominalOutputVoltage(+0.0f, -0.0f);
-        motor.configPeakOutputVoltage(+12.0, -12.0);
-        motor.setFeedbackDevice(FeedbackDevice.QuadEncoder);
+        motor_A.config_kP(0, 1.0, 0);
+        motor_A.config_kI(0, 0.0, 0);
+        motor_A.config_kD(0, 0.0, 0);
+        motor_A.config_kF(0, 0.0, 0);
+
+        motor_A.setNeutralMode(NeutralMode.Coast);
+        motor_A.configNominalOutputVoltage(+0.0f, -0.0f);
+        motor_A.configPeakOutputVoltage(+12.0, -12.0);
+        motor_A.setFeedbackDevice(FeedbackDevice.QuadEncoder);
 
     }
 
@@ -56,7 +59,7 @@ public class Shoulder extends Subsystem {
         brake.set(true); // set the brake.
 
         // zero the position.
-        motor.setSelectedSensorPosition(STARTING_POS);
+        motor_A.setSelectedSensorPosition(STARTING_POS);
 
         set(STARTING_POS);
     }
@@ -67,11 +70,11 @@ public class Shoulder extends Subsystem {
     }
 
     public boolean IsAtSetpoint() {
-        return Math.abs(motor.getPosition() - m_pos) < Shoulder.IN_POSITION_SLOP;
+        return Math.abs(motor_A.getPosition() - m_pos) < Shoulder.IN_POSITION_SLOP;
     }
 
     public int get() {
-        return (int) motor.getPosition();
+        return (int) motor_A.getPosition();
     }
 
     public void initDefaultCommand() {
@@ -83,52 +86,56 @@ public class Shoulder extends Subsystem {
         SmartDashboard.putBoolean("Shoulder Brake", brake.get());
     }
 
-
+    public boolean percentState = false; 
     public void periodic() {
-        switch (m_state) {
+        
+        // switch (m_state) {
+        
 
-        case STOPPED:
-            // when we are stopped, set the motor 0 power.
-            this.motor.set(ControlMode.PercentOutput, 0);
-            // set the brake.
-            this.brake.set(true);
-            break;
+        // case STOPPED:
+        //     // when we are stopped, set the motor 0 power.
+        //     this.motor_A.set(ControlMode.PercentOutput, 0);
+        //     // set the brake.
+        //     this.brake.set(true);
+        //     break;
 
-        case START_MOVING:
-            // release the brake
-            this.brake.set(false);
-            // start a timer
-            m_startTimer = m_timer.get();
-            m_state = State.WAIT_FOR_BRAKE_RELEASE;
-            break;
+        // case START_MOVING:
+        //     // release the brake
+        //     this.brake.set(false);
+        //     // start a timer
+        //     m_startTimer = m_timer.get();
+        //     m_state = State.WAIT_FOR_BRAKE_RELEASE;
+        //     break;
 
-        case WAIT_FOR_BRAKE_RELEASE:
-            // when the timer expires, set the motor to the desired position.
-            if (m_timer.get() - m_startTimer >= BRAKE_RELASE_TIME_SEC) {
-                motor.set(ControlMode.Position, m_pos);
-            }
-            break;
+        // case WAIT_FOR_BRAKE_RELEASE:
+        //     // when the timer expires, set the motor to the desired position.
+        //     if (m_timer.get() - m_startTimer >= BRAKE_RELASE_TIME_SEC) {
+        //         motor_A.set(ControlMode.Position, m_pos);
+        //     }
+        //     break;
 
-        case MOTOR_MOVING:
-            // If we are close to position...
-            if (Math.abs(get() - m_pos) < IN_POSITION_SLOP) {
-                // turn off the motor.
-                this.motor.set(ControlMode.PercentOutput, 0);
-                // set the brake
-                this.brake.set(true);
-                m_state = State.STOPPED;
-            }
-            break;
-        }
-    
-        if(Robot.oi.pivotArmPower() != 0.0){
-            this.brake.set(false);
-            this.motor.set(ControlMode.PercentOutput, Robot.oi.pivotArmPower());
-            percentState = true;
-        }else if(percentState) {
-            m_state=State.STOPPED;
-            percentState = false;
-        }
+        // case MOTOR_MOVING:
+        //     // If we are close to position...
+        //     if (Math.abs(get() - m_pos) < IN_POSITION_SLOP) {
+        //         // turn off the motor.
+        //         this.motor_A.set(ControlMode.PercentOutput, 0);
+        //         // set the brake
+        //         this.brake.set(true);
+        //         m_state = State.STOPPED;
+        //     }
+        //     break;
+        // }
+
+        // if(Robot.oi.pivotArmPower() != 0.0 ){
+        //     this.brake.set(false);
+        //     this.motor_A.set(ControlMode.PercentOutput, Robot.oi.pivotArmPower());
+            // percentState = true;
+        // } 
+        // else
+        // if(percentState) {
+        //     m_state=State.STOPPED;
+        //     percentState = false;
+        // }
 
     }
 }
